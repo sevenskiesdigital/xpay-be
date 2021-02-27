79d1eb90458fa01d2a84d6228c289ddcdb4f8def
bofandra_dans <bofandra@dansmultipro.com>
2021-02-27 10:28:01 +0700

    fix get seller orders

--- order/order.services.js
+++ order/order.services.js
@@ -31,7 +31,7 @@ module.exports = {
                 if(error){
                     return callBack(error)
                 }
-                return callBack(null, results[0])
+                return callBack(null, results)
             }
         );
     }
